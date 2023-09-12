c1429b0e65bc189d41ad2965811beec94832035b
VelizhaninPavel <velizhaninpavel@gmail.com>
2023-09-12 12:37:36 +0500

    Task1

---
+++ Tyuiu.VelizhaninPI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VelizhaninPI.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+            string name = "Pavel";
+            string age = "18";
+            Console.WriteLine("Hello, my name is " + name + " and i am " + age + " years old.");
+            Console.ReadKey();
+        }
+    }
+}
