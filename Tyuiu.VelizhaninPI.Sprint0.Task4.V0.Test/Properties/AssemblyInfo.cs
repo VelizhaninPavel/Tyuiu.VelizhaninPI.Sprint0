fdee817b596e93a10ae84af4262ffff5ceab19cb
VelizhaninPavel <velizhaninpavel@gmail.com>
2023-09-16 19:54:52 +0500

    Task4

---
+++ Tyuiu.VelizhaninPI.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VelizhaninPI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.VelizhaninPI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf10dd38-7e50-499d-b2b2-09361704e481")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VelizhaninPI.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VelizhaninPI.Sprint0.Task4.V0.Lib;
+using Tyuiu.VelizhaninPI.Sprint0.Task4.V0.Test;
+
+namespace Tyuiu.VelizhaninPI.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.ReadKey();
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.ReadKey();
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.ReadKey();
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+
+        }
+    }
+}
